7b60b0160c3b093b23068f5fa0da2a0818aa956a
eduardruzga <wonderwhy.er@gmail.com>
2024-05-12 21:38:57 +0300

    Update from esprima to espree for never JS version syntax support

--- serverModules/checkjs.js
+++ serverModules/checkjs.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const esprima = require('esprima');
+const espree = require('espree');
 function checkJavaScriptFile(filePath) {
     return new Promise((resolve, reject) => {
         const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
         try {
-            esprima.parseScript(fileContent);
+            espree.parse(fileContent, {
+                ecmaVersion: "latest", // or whichever ECMAScript version you are targeting
+                loc: true  // Enable line/column location information
+            });
             resolve([]); // No syntax errors
         } catch (error) {
-            resolve([{ line: error.lineNumber, message: error.description }]);
+            resolve([{ line: error.lineNumber, column: error.column, message: error.message }]);
         }
     });
 }
